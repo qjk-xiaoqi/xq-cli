0ac83ae596af6ad98bf71c127fb7c3b0fff2bd88
qjk-xiaoqi <47919141+qjk-xiaoqi@users.noreply.github.com>
2020-09-15 16:00:01 +0800

    Update build.js

--- src/build.js
+++ src/build.js
@@ -6,6 +6,8 @@ const config = require('./webpack.build.js');
 let build = () => {
     // 打包
     webpack(config, function(err, stats) {
+         // 这个回调函数是webpack编译过程中执行
+        // process.stdout用来控制标准输出，也就是在命令行窗口向用户显示内容。它的write方法等同于console.log
         process.stdout.write(stats.toString({
             colors: true,
             modules: false,
